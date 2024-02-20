416a6a7625e17ccd2c7c70d7976d0b8d37da129e
Sami Hammami <samih@lablup.com>
2024-02-21 00:39:10 +0900

    add assests and Main page content

---
+++ components/index.ts
@@ -0,0 +1,9 @@
+// export all our components so we can import them in other pages
+
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+
+export {
+    Hero,
+    CustomButton
+}
