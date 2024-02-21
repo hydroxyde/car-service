83fe2931dd635df3c582224bf7cf98a76738f22a
Sami Hammami <samih@lablup.com>
2024-02-21 19:11:50 +0900

    add modal for car details

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ export async function fetchCars() {
         'X-RapidAPI-Key': 'bf7f4ab01cmsh9129b4f4e9b4fb9p1a8098jsnefef0fda3d2f',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { headers: headers })
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', { headers: headers })
 
     const result = await response.json()
 
