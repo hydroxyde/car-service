bb03bed20cd7663f644402776075b73a73b692a3
Sami Hammami <samih@lablup.com>
2024-02-21 18:45:48 +0900

    add car cards

--- components/index.ts
+++ components/index.ts
@@ -7,6 +7,8 @@ import Footer from './Footer';
 import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
 
 export {
     Hero,
@@ -15,5 +17,7 @@ export {
     Footer,
     SearchBar,
     CustomFilter,
-    SearchManufacturer
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
 }
