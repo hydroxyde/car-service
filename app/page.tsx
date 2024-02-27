2372644f8f657e107c0714efa5b9b438f89591d6
Sami Hammami <samih@lablup.com>
2024-02-27 19:10:31 +0900

    add Show more button layout and props

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import { Hero, SearchBar, CustomFilter, CarCard, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
@@ -38,6 +38,12 @@ export default async function Home({ searchParams }) {
               {allCars?.map((car) => <CarCard
                 car={car} />)}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.pageNumber || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+
+            />
           </section>
         ) : (
           <div className="home__error-container">
